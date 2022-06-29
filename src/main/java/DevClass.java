a04d2c0067a65a97ee9b1ca26f534797e9bdd912
Chinmayraorane <91417963+Chinmayraorane@users.noreply.github.com>
2022-06-29 15:46:04 +0530

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass stream2");
+        System.out.println("DevClass-stream2");
         //checking from stream2 
     }
 }
